45e5cc015ec68528fa1c54c8266579890e53d11e
Ralfs Karlis Lapers <ralfs.lapers@gmail.com>
2020-11-21 17:07:25 +0200

    meow3

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>meow moew</Text>
+      <Text>meow moew meow</Text>
       <StatusBar style="auto" />
     </View>
   );
